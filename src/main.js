29796a0016500e2fa116f208c3d08b534350ebe8
Gavin Allmond <GavinA@rp.local>
2021-08-28 19:51:45 +0100

    changed export format

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const CycleParking = require("./cycleParking/CycleParking");
 const ExportManager = require("./cycleParking/ExportManager");
 const LocalStore = require("./cycleParking/LocalStore");
 
-exports = {
+export {
   CycleParking,
   ExportManager,
   LocalStore
