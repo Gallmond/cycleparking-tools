1a22632bcfaf036670ca919e6d4316f932493363
Gavin Allmond <GavinA@rp.local>
2021-08-28 19:47:58 +0100

    added missing main

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const CycleParking = require("./cycleParking/CycleParking");
+const ExportManager = require("./cycleParking/ExportManager");
+const LocalStore = require("./cycleParking/LocalStore");
+
+module.exports = {
+  CycleParking: CycleParking,
+  ExportManager: ExportManager,
+  LocalStore: LocalStore
+}
